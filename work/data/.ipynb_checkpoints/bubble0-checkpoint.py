c3bc6b3412a2fd1e805dbaed2f513d67e5db4634
Seiya Kawamoto <kid960805@gmail.com>
2021-11-05 15:45:59 +0900

    add: [WIP]bubble sync

---
+++ work/data/.ipynb_checkpoints/bubble0-checkpoint.py
@@ -0,0 +1,7 @@
+import math
+
+Rref = 1.0 * math.exp(-5) # reference_radius
+Pref = 2.500 * math.exp(3) # reference_pressure
+sigma = 7.275 * math.exp(-2) # surface_tension
+kappa = 1.666667 # specific_heat_ratio
+Pv = 2.337 * math.exp(3) # vapor pressure
---
+++ work/data/.ipynb_checkpoints/liquid_data-checkpoint.py
@@ -0,0 +1,5 @@
+import math
+
+Rho_L = 998.2 # liquid_density
+Pinf = 1.0 * math.exp(5) # pressure_at_infinity
+Va = 1478.0 # speed_of_sound
